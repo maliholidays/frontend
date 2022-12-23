344c7a7907ac64f6c6df3a166999cd8f27cb8364
maliholidays <maliholidays45@gmail.com>
2022-12-23 12:48:54 +0530

    Contact Page

---
+++ src/Containers/User/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Contact/Banner/Banner'
+import ContactSection from '../components/Contact/ContactSection/ContactSection'
+
+function Contact() {
+  return (
+    <div>
+        <Banner/>
+        <ContactSection/>
+    </div>
+  )
+}
+
+export default Contact
