840a8ea681831e41dca666d0b6c3a4557ac8ce1b
maliholidays <maliholidays45@gmail.com>
2022-12-23 12:40:36 +0530

    Page Place Added

---
+++ src/Containers/User/pages/Place.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Places/Banner/Banner'
+import Search from '../components/Places/Search/Search'
+
+function Place() {
+  return (
+    <div>
+        <Banner/>
+        <Search/>
+    </div>
+  )
+}
+
+export default Place
