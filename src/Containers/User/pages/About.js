5ceaca8c9771038467e81e493b7dfe9958c2a3e5
maliholidays <maliholidays45@gmail.com>
2022-12-23 12:48:42 +0530

    About Page

---
+++ src/Containers/User/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/About/Banner/Banner'
+
+function About() {
+  return (
+    <div>
+        <Banner/>
+    </div>
+  )
+}
+
+export default About
