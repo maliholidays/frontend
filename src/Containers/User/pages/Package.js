52238e8a1901ed51531be074993fc05352fda4c2
maliholidays <maliholidays45@gmail.com>
2022-12-23 12:40:48 +0530

    Page Package Added

---
+++ src/Containers/User/pages/Package.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Packages/Banner/Banner'
+import Search from '../components/Packages/Search/Search'
+
+function Package() {
+  return (
+    <div>
+        <Banner/>
+        <Search/>
+    </div>
+  )
+}
+
+export default Package
