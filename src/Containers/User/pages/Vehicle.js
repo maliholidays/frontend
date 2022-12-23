aabe641b84e6a2ea56371009f5b3f55cf1c64021
maliholidays <maliholidays45@gmail.com>
2022-12-23 12:40:14 +0530

    Page Vehicle Added

---
+++ src/Containers/User/pages/Vehicle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Vehicle/Banner/Banner'
+import Vehicles from '../components/Vehicle/Vehicles/Vehicles'
+
+function Vehicle() {
+  return (
+    <div>
+        <Banner/>
+        <Vehicles/>
+    </div>
+  )
+}
+
+export default Vehicle
